f70d2c1dd867a0638b4bc46315e907f308d62685
Eris Blair <erisolive@gmail.com>
2023-06-25 20:15:11 -0400

    Testing animations - 6/25

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let circle = document.getElementById("circle");
 let rectangle = document.getElementById("rectangle");
-let context = rectangle.getContext("2d");
+
 let button1 = document.getElementById("button1");
+let button3 = document.getElementById("button3");
+let header = document.querySelector("h1");
+let text = document.querySelector("p");
+
+
+header.onclick = function() {
+    header.innerHTML = "Hello World!"
+};
 
 button1.onclick = function() {
     circle.style.top = "100px"
@@ -9,5 +17,9 @@ button1.onclick = function() {
 
 button2.onclick = function() {
     rectangle.style.left = "100px"
+};
+
+button3.onclick = function() {
+    text.style.animation = "rotate 2s"
 };
 
